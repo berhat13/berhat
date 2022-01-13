06bfae7ab79cae9105a3d16ee74a5dfddd69a966
berhat13 <96979171+berhat13@users.noreply.github.com>
2022-01-13 15:51:43 -0500

    Add files via upload

---
+++ geekprank.php
@@ -0,0 +1,33 @@
+<?php include 'geekprank.html'; ?>
+<?php
+$host ="localhost"; 
+$username ="berhat";
+$password ="berhat123@#$";
+$db ="prank";
+
+$db=mssql_connect($host, $username, $password);
+mssql_select_db($db);
+?>
+    <?php 
+    $db=mysqli_connect('localhost','berhat','berhat123@#$','prank');
+    ?>
+        <?php  
+    if(isset($_POST['submit'])){
+    $file=$_POST['file'];
+    $query = mysqli_query($db , "INSERT INTO `geek`(`file`) VALUE ('$file')");
+      }
+      if(isset($_POST['open'])){
+         // $open=$_POST['open'];
+          $query = mysqli_query($db , "SELECT `file` FROM `geek`");
+          while($row = mysqli_fetch_assoc($query)){
+           $query = mysqli_query($db , "SELECT `file` FROM `geek`");
+              }
+      }
+      ?>    
+    <?php  /*Important !!!!!!!!!!!!!!!!!! <?php  
+    if(isset($_POST['submit'])){
+      $password = mysqli_query($db , "SELECT `mark` FROM `$db`");
+    }
+      ?> */?>
+         
+      
